ffe9cdeac9294e62835a08a016a4835378b5643e
MichalNerdCow <79836696+MichalNerdCow@users.noreply.github.com>
2021-08-09 13:17:33 +0200

    Contact page

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -56,15 +56,14 @@ function handleError({values, error, reset}) {
 }
 
 function handleSuccess({values, reset, confirmations}) {
-    const formWrapper = document.getElementsByClassName('contact__form')
-	const form = formWrapper[0].children[0]
-	const confirmation = formWrapper[0].children[1]
+	const form = document.getElementsByClassName('gform_wrapper')
+	const confirmation = document.getElementsByClassName('contact__confirmation')
 
 	const customerName = document.getElementsByClassName('customer-name')
 
-	form.classList.add('hidden')
+	form[0].classList.add('hidden')
 	customerName[0].innerText = values["input_1"]
-	confirmation.classList.add('visible')
+	confirmation[0].classList.add('visible')
 
 	animateScrollTo(0)
 }
